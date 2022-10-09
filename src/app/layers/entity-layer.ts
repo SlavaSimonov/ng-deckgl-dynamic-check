3ca260f5271be2338c14aaba2f031f780161a040
Slava Simonov <simonowslawa@gmail.com>
2022-10-09 20:31:26 +0300

    Small fix

--- src/app/layers/entity-layer.ts
+++ src/app/layers/entity-layer.ts
@@ -3,8 +3,6 @@ import { ScatterplotLayer, TextLayer, IconLayer, PathLayer } from "@deck.gl/laye
 import { SelectedColor } from "../consts/entity-colors";
 import { IconMappingNames, ICON_MAPPING } from "../consts/icon-mapping";
 import { EntityBinData } from "../models/entity-bin-data";
-import icon from "../../assets/"
-
 
 
 interface EntityProps {
